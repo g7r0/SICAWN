f1706a45ee2bc7a844b6368ed662607fc6a0f29a
Jair Ismael Guerrero Luna <minecitachato@gmail.com>
2026-08-21 00:41:58 -0600

    Correcciones CU_02

--- controllers/LogoutController.php
+++ controllers/LogoutController.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+require_once __DIR__ . '/../config/rutas.php';
+
 session_destroy();
-header('Location: /controllers/LoginController.php');
+header('Location: ' . BASE_URL . '/controllers/LoginController.php');
 exit;
